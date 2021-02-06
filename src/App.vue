f3c73f020b6161ea2ba5c2434d89ff9bf5353a12
nam tran <namsir@gmail.com>
2021-02-06 12:10:52 -0500

    define styles for fonts mobile and desktop

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,12 @@
 <template>
-
+  <h1>Heading 1</h1>
+  <h2>Heading 2</h2>
+  <h3>Heading 3</h3>
+  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eaque eius ex incidunt inventore ipsum laudantium magni porro praesentium rem voluptatem.</p>
 </template>
 
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+
 </script>
 
 <style>
