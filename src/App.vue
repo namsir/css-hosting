736c9adb16572020c05b22ba424b154912cd12e8
nam tran <namsir@gmail.com>
2021-02-06 11:23:28 -0500

    init css project

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<template>
+
+</template>
+
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<style>
+@import "assets/css/normalize.css";
+@import "assets/css/style.css";
+</style>
