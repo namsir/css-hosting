4dc529d49e17fa159f0cac65171f7b8831fd5dac
nam tran <namsir@gmail.com>
2021-02-06 12:26:50 -0500

    build arrow link component

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,5 @@
 <template>
-  <h1>Heading 1</h1>
-  <h2>Heading 2</h2>
-  <h3>Heading 3</h3>
-  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eaque eius ex incidunt inventore ipsum laudantium magni porro praesentium rem voluptatem.</p>
+  <a href="#" class="link-arrow">Learn more</a>
 </template>
 
 <script setup>
