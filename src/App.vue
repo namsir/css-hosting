462aadc497e51b755076f04a3701b0c2481fce64
Nam Tran <namsir@gmail.com>
2021-02-08 08:31:21 -0500

    creating button-group

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,13 @@
 <template>
+  <div class="input-group">
+    <span class="icon-container">
+  <svg class="icon icon--primary">
+    <use xlink:href="./assets/images/sprite.svg#wordpress"></use>
+  </svg>
+</span>
+    <input type="text" class="input" placeholder="Enter domain here..."/>
 
+  </div>
 </template>
 
 <script setup>
